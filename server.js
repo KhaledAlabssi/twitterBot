7080c88e7f47a3c3485da3d4c7682306c8d33834
KhaledAlabssi <khaled.absi5@gmail.com>
2022-10-30 19:16:40 +0100

    generate quotes from json file

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 import rwClient from "./twitterClinet.js";
 import cron from 'node-cron'
+import axios from "axios";
+import qoutes from './quotes.json' assert {type: "json"};
 
 const tweet = async () => {
     try {
-        const m = await rwClient.v1.uploadMedia('./me.jpg')
-        await rwClient.v1.tweet("Bot test", {media_ids: m})
+        const q = qoutes[Math.floor(Math.random()*qoutes.length)]
+        await rwClient.v1.tweet(`${q.text} \n \n${q.author && q.author}`)
 
 
     } catch (error) {
@@ -13,11 +15,24 @@ const tweet = async () => {
     }
 }
 
-tweet();
+// Media tweet
 
-// cron.schedule('* 10 * * *', () => {
+// const MediaTweet = async () => {
+//   try {
+//     const m = await rwClient.v1.uploadMedia("./me.jpg")
+//     await rwClient.v1.tweet("Bot test", {
+//       media_ids: m,
+//     })
+//   } catch (error) {
+//     console.error(error)
+//   }
+// }
 
-//     tweet();
-// })
+// tweet();
+
+cron.schedule('* 8 * * *', () => {
+
+    tweet();
+})
 
 
