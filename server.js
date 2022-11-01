609dcf071554357b703e3df3abdce4a67decd091
KhaledAlabssi <khaled.absi5@gmail.com>
2022-11-01 09:11:33 +0100

    change the time of the tweets

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ import qoutes from './quotes.json' assert {type: "json"};
 const tweet = async () => {
     try {
         const q = qoutes[Math.floor(Math.random()*qoutes.length)]
-        await rwClient.v1.tweet(`${q.text} \n \n${q.author && q.author}`)
+        await rwClient.v1.tweet(`${q.text}\n\n${q.author && q.author}`)
 
 
     } catch (error) {
@@ -30,7 +30,7 @@ const tweet = async () => {
 
 // tweet();
 
-cron.schedule('* 8 * * *', () => {
+cron.schedule('1 29 7 * * *', () => {
 
     tweet();
 })
